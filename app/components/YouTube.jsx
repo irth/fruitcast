0786db53040b2cfb857d3ebd157e729fd2fe67b8
Wojciech Kwolek <me@irth.pl>
2017-04-17 19:24:42 +0200

    Allow to play/pause/stop YouTube videos
    * moved from an iframe to https://github.com/troybetz/react-youtube
    
    Closes #1

--- app/components/YouTube.jsx
+++ app/components/YouTube.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 
+import ReactYouTube from 'react-youtube';
+
 const style = {
   player: {
     width: '100%',
@@ -11,15 +13,50 @@ const style = {
 };
 
 export default class YouTube extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {player: null};
+  }
+
+  componentWillReceiveProps(newProps) {
+    this.syncPlayerState(newProps.state);
+  }
+
   render() {
-    return <h1>{this.props.state}</h1>;
     return (
-      <iframe
-        style={style.player}
-        src={
-          `https://youtube.com/embed/${this.props.id}?autoplay=1&controls=0&disablekb=1&enablejsapi=1`
-        }
+      <ReactYouTube
+        opts={{
+          height: '100%',
+          width: '100%',
+          playerVars: {
+            disablekb: 1,
+            controls: 0,
+          },
+        }}
+        videoId={this.props.id}
+        onReady={this._onReady.bind(this)}
       />
     );
+  }
+
+  syncPlayerState(state) {
+    const player = this.state.player;
+    if (this.state.player == null) return;
+    switch (state) {
+      case 'playing':
+        player.playVideo();
+        break;
+      case 'paused':
+        player.pauseVideo();
+        break;
+      case 'stopped':
+        player.stopVideo();
+        break;
+    }
+  }
+
+  _onReady(e) {
+    this.setState({player: e.target});
+    this.syncPlayerState(this.props.state);
   }
 }
