d2be3475be00727256a7cefa519829d6d728a159
Wojciech Kwolek <me@irth.pl>
2017-04-18 14:55:36 +0200

    Add an API for retrieving the NASA APOD.
    Astronomic picture of the day.
    
    * Allow specifying NASA API key via NASA_API_KEY env variable
    * Add an API call that returns cached APOD data

--- api.js
+++ api.js
@@ -1,3 +1,7 @@
+const fetch = require('node-fetch');
+let NASA_APOD = [0, ''];
+const NASA_API_KEY = process.env.NASA_API_KEY || 'DEMO_KEY';
+
 module.exports = client => ({
   post: {
     '/stream/youtube': data => client.startStreaming('youtube', data),
@@ -8,5 +12,19 @@ module.exports = client => ({
   },
   get: {
     '/': () => 'Welcome to FruitCast!',
+    '/nasa/apod': () => {
+      const time = Date.now();
+      if (time - NASA_APOD[0] > 3600e3) {
+        return fetch(
+          `https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}`
+        ).then(response => {
+          console.log('HELLO');
+          return response.json();
+        }).then(data => {
+          NASA_APOD = [time, data];
+          return Promise.resolve(data);
+        });
+      } else return NASA_APOD[1];
+    },
   },
 });
